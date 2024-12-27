ec1a07dc905919ece8a55422989779cfaa65ad45
cironis <ciroshia@gmail.com>
2024-12-27 03:02:51 +0000

    Updating app.py

--- views/home.py
+++ views/home.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Dash do Fino", page_icon="♙", layout="wide")
 
 @st.cache_data
 def carregar_base():
-    base = pd.read_csv("/bases/extrato_fi.csv")
+    base = pd.read_csv("bases/extrato_fi.csv")
     return base
 
 base_df = carregar_base()
