f902ac20869e4d0ad38413a048501fcb3d69c72d
cironis <ciroshia@gmail.com>
2024-12-27 03:33:21 +0000

    Updating app.py

--- views/home.py
+++ views/home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(page_title="Dash do Fino", page_icon="♙", layout="wide")
 
@@ -11,6 +12,8 @@ def carregar_base():
 base_df = carregar_base()
 
 st.title("Dash de Exemplo para o Fino")
+
+st.subheader("Exemplo de filtros e Tabelas")
 
 admins = base_df["ADMIN"].unique()
 
@@ -25,4 +28,21 @@ coluna_dados_select = st.multiselect("Selecione a coluna de Dados", columns_with
 grouby_data = base_df.groupby(["DT_COMPTC","DENOM_SOCIAL"])[coluna_dados_select].sum().reset_index()
 
 st.dataframe(grouby_data,hide_index=True,use_container_width=True)
-st.write(grouby_data.columns)
+
+st.subheader("Exemplo de Gráfico")
+
+coluna_grafico_select = st.selectbox("Selecione a Coluna para o Gráfico", columns_with_tab)
+
+grouby_graph = base_df.groupby(["DENOM_SOCIAL"])[coluna_grafico_select].sum().reset_index()
+
+fig = px.bar(
+    grouby_graph,
+    x="DENOM_SOCIAL",  # X-axis
+    y=coluna_grafico_select,  # Y-axis is user-selected
+    title=f"Bar Graph of {coluna_grafico_select} by DENOM_SOCIAL",
+    labels={"DENOM_SOCIAL": "Denominação Social", coluna_grafico_select: "Valor"},
+    text_auto=True  # Automatically add text labels to bars
+)
+
+# Display the chart in Streamlit
+st.plotly_chart(fig)
